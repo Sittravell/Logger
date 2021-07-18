0e2f26cb82feb6cc841dd6d73e1ccc5a44e0c512
Sittravell <37928570+Sittravell@users.noreply.github.com>
2021-07-19 03:49:48 +0800

    Update Logger.kt

--- Logger.kt
+++ Logger.kt
@@ -1,3 +1,4 @@
+/* REPLACE: With your application id */
 package com.example.templates.utils
 
 import android.util.Log
@@ -6,5 +7,17 @@ interface RequiresLogger{
     val TAG: String
     fun log(message: String){
         Log.d(TAG, message)
+    }
+}
+
+/* HOW TO USE? */
+/* EXAMPLE: */
+class ExampleClass: RequiresLogger{
+    /* IMPORTANT: define the variable TAG with anything (preferably your class name) */
+    override val TAG = "ExampleClass"
+    
+    fun exampleFunction(){
+        /* NOTE: This is how you log anything */
+        log("exampleFunction is being executed")
     }
 }
